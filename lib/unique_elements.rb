a585b66218ff0870a6443826c8d88149c6960c17
Michael <michaeljducey@gmail.com>
2016-06-15 00:04:23 +0000

    Submitting work for find only unique elements

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,8 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+    array_duplicate = Array.new
+    array_duplicate = arr.detect{ |element| arr.count(element) >= 2}
+    arr.delete(array_duplicate)
+    return arr
 end
